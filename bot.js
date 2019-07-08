c717ba9279d94797c7b38d06312b82ef40c5e3ab
zabzob123 <52677986+zabzob123@users.noreply.github.com>
2019-07-08 22:05:46 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message',message => {
+    if (message.content === 'ping') {
+      message.reply('pong');
+    }
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.NTk3ODc5NzU1MDk3ODk5MTA4.XSOsvQ.3yLKlLEd_gLlKD1C3ks4P7DM7bU);
